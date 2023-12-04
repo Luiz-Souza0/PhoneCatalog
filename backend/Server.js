7bcac6d4320f0168a90699ac20b708f8caeb4e87
Luiz-Souza0 <114114974+Luiz-Souza0@users.noreply.github.com>
2023-12-04 14:22:39 -0300

    Updating

--- backend/Server.js
+++ backend/Server.js
@@ -12,7 +12,7 @@ const PhoneRoute = require('./route/PhoneRoute');
 
 app.use(cors({ origin: '*', credentials: false }));
 
-mongoose.connect('mongodb+srv://luiz_0:ditavia@cluster0.z9e6l1j.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://user:<password>@cluster0.z9e6l1j.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 app.use('/', PhoneRoute);
 
